681a0b2745421d0dfe82dabed2b1b0d2f359c2ce
Stanleyhenrique <alunostanleyhenrique@hotmail.com>
2021-10-18 11:43:37 -0300

    Primeiros desafios em C# e Javascript

---
+++ Csharp/Desafio-matematicos-2/de-Desafios-com-C#-São-ou-não-multiplos/math.cs
@@ -0,0 +1,17 @@
+using System;
+
+ class minhaClasse {
+        static void Main(string[] args) {
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            if (A %   ==  ||  % A == ) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
